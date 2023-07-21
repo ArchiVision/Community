32fa4dd7ba4ac057ce01965767fe0af509424b8e
overpathz <overpathz@gmail.com>
2023-07-21 15:08:52 +0300

    Refactor & improve

--- src/main/java/com/archivision/community/strategy/inputstate/impl/MatchStateHandler.java
+++ src/main/java/com/archivision/community/strategy/inputstate/impl/MatchStateHandler.java
@@ -1,9 +1,7 @@
 package com.archivision.community.strategy.inputstate.impl;
 
 import com.archivision.community.bot.State;
-import com.archivision.community.document.User;
 import com.archivision.community.matcher.MatchedUsersListResolver;
-import com.archivision.community.matcher.model.UserWithMatchedProbability;
 import com.archivision.community.messagesender.MessageSender;
 import com.archivision.community.service.KeyboardBuilderService;
 import com.archivision.community.service.UserService;
@@ -12,8 +10,6 @@ import com.archivision.community.util.InputValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
-
-import java.util.List;
 
 @Component
 @Slf4j
@@ -29,8 +25,6 @@ public class MatchStateHandler extends AbstractStateHandler {
 
     @Override
     public void handle(Message message) {
-
-        // todo: send by one
 
     }
 
--- src/main/java/com/archivision/community/strategy/message/impl/PhotoMessageStrategy.java
+++ src/main/java/com/archivision/community/strategy/message/impl/PhotoMessageStrategy.java
@@ -1,5 +1,9 @@
 package com.archivision.community.strategy.message.impl;
 
+import com.archivision.community.bot.State;
+import com.archivision.community.document.User;
+import com.archivision.community.service.StateManagerService;
+import com.archivision.community.service.UserService;
 import com.archivision.community.strategy.message.MessageStrategy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +14,24 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 @Slf4j
 @RequiredArgsConstructor
 public class PhotoMessageStrategy implements MessageStrategy {
+    private final StateManagerService stateManagerService;
+    private final UserService userService;
     @Override
     public void handleMessage(Message message) {
+        Long chatId = message.getChatId();
+        userService.getUserByTelegramId(chatId)
+                .ifPresentOrElse(user -> {
+                    if (ifNotInPhotoState(user)) return;
+                    stateManagerService.manageOtherStates(user.getState(), message);
+                }, () -> log.info("User with tg id={} is not registered in DB", chatId));
+    }
 
+    private static boolean ifNotInPhotoState(User user) {
+        if (!user.getState().equals(State.PHOTO)) {
+            log.info("User={} in state={}. Do not process picture", user.getTelegramUserId(), user.getState());
+            return true;
+        }
+        return false;
     }
 
     @Override
