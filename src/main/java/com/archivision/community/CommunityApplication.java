ff212d3d70a6840d6f1cc07524cd9c86d2dd9885
Serwio YK <56473255+Serwios@users.noreply.github.com>
2024-04-14 22:46:57 +0300

    Yaroslav's refactoring

--- src/main/java/com/archivision/community/CommunityApplication.java
+++ src/main/java/com/archivision/community/CommunityApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class CommunityApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(CommunityApplication.class, args);
 	}
 }
