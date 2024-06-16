69861fca46235146f4099911c53f63b6447b76ae
Serwio YK <56473255+Serwios@users.noreply.github.com>
2024-06-16 18:36:57 +0300

    Added rabbitmq to compose

--- src/main/java/com/archivision/community/bot/CommunityBot.java
+++ src/main/java/com/archivision/community/bot/CommunityBot.java
@@ -1,6 +1,8 @@
 package com.archivision.community.bot;
 
 import com.archivision.community.processor.UpdateProcessor;
+import jakarta.annotation.PostConstruct;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -14,7 +16,14 @@ public class CommunityBot extends TelegramLongPollingBot {
     private String tgUsername;
     @Value("${telegram.bot.token}")
     private String tgToken;
+    @Setter
     private UpdateProcessor updateProcessor;
+
+    @PostConstruct
+    public void init() {
+        log.info("Bot username: {}", tgUsername);
+        log.info("Bot token: {}", tgToken);
+    }
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -29,9 +38,5 @@ public class CommunityBot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return tgToken;
-    }
-
-    public void setUpdateProcessor(UpdateProcessor updateProcessor) {
-        this.updateProcessor = updateProcessor;
     }
 }
